ed3799ecd284b5fe973a3fd37b48907e7e3416b8
camila babo <96503984+camilababo@users.noreply.github.com>
2022-05-23 17:27:38 +0100

    teste: se os packages estão corretos

--- package_aab/automata/unittest_automata.py
+++ package_aab/automata/unittest_automata.py
@@ -1,15 +1,18 @@
 import unittest
 from automata import Automata
 
+
 class testAutomata(unittest.TestCase):
     def setUp(self):
         self.automata = Automata("AC", "ACA")
 
+
 class testAutomataMethods(testAutomata):
     def runTest(self):
-        self.assertEqual(self.automata.patternSeqPosition("CACAACAA"), [1, 4],"lista de posições errada" )
+        self.assertEqual(self.automata.patternSeqPosition("CACAACAA"), [1, 4], "lista de posições errada")
         self.assertEqual(self.automata.applyNextState("CACAACAA"), [0, 0, 1, 2, 3, 1, 2, 3, 1],
                          "lista de próximos estados errada")
+
 
 if __name__ == '__main__':
     unittest.main()
