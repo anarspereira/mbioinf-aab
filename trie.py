3be976f8599f12f2a4df4c56cfb73441f1f08dc9
Ana Rafaela Pereira <98289507+anarspereira@users.noreply.github.com>
2022-05-23 03:01:14 +0100

    trie completa. to-do: adicionar mais comentários

--- trie.py
+++ trie.py
@@ -3,12 +3,14 @@
 """
 Class: Trie
 """
+
+#TODO: adicionar mais comentários
+
 class Trie:
 
     def __init__(self):
         """
         Método que guarda os valores utilizados nos restantes métodos
-
         :param nodes: guarda um dicionário de nodes
         :param num: guarda nº do último nó criado
         """
@@ -35,7 +37,7 @@ class Trie:
     def add_pattern(self, p):
         """
         Método que adiciona padrão à trie
-        :param p: padrão a adicionar
+        :param p: padrão, começa sempre na root
         """
         position = 0
         node = 0
@@ -51,7 +53,7 @@ class Trie:
         :param pats: padrões
         """
         for p in pats:
-            self.add_pattern()
+            self.add_pattern(p)
 
     def prefix_trie_match(self, text: str) -> list:
         """
@@ -59,32 +61,32 @@ class Trie:
         Percorre a árvore (saindo do node raiz) e segue os arcos correspondentes.
         Se atingir uma folha, o padrão correspondente a esta folha é prefixo da sequência.
         :param text: sequência
-        :return:
+        :return: match ou nada
         """
         position = 0 # posição inicial
-        match = "" # match inicial (vazio)
+        match = "" # match inicial (resultado)
         node = 0 # nó inicial
         while position < len(text): # enquanto a posição for menor que o tamanho do texto,
             if text[position] in self.nodes[node].keys():
                 node = self.nodes[node][text[position]]
                 match += text[position]
-                if self.nodes[node] == {}:
-                    return match
+                if self.nodes[node] == {}: # se atingir uma folha,
+                    return match # só dá match nesse caso
                 else:
-                    position += 1
-            return None
-
+                    position += 1 # próxima posição
+            else:
+                return None
         return None
 
     def trie_matches(self, text: str) -> list:
         """
         Método que identifica matches dos padrões em toda a sequência text.
         Processo iterativo - faz match da sequência, remove o primeiro símbolo desta, repete o processo -> o processo anterior é repetido para todos os sufixos da seq.
-        :param text:
-        :return:
+        :param text: sequência
+        :return: lista de matches
         """
         res = []
-        for i in range(len(text)):
+        for i in range(len(text)): # para cada posição na sequência text,
             m = self.prefix_trie_match(text[i:])
             if m != None:
                 res.append((i, m))
