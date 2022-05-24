cf891c837299c656a2c1bcb1c34d6b5e38968b88
camila babo <96503984+camilababo@users.noreply.github.com>
2022-05-24 17:19:44 +0100

    tirei os unittests do ficheiro do automata.py porque já estavam no test_automata.py. cirei diretório para o my_seq.py

--- package_aab/automata/automata.py
+++ package_aab/automata/automata.py
@@ -105,30 +105,5 @@ def overlap(seq1: str, seq2: str) -> int:
             return i
     return 0 #se não houver match devolve um zero
 
-class testAutomata(unittest.TestCase):
-    def setUp(self):
-        self.automata = Automata("AC", "ACA")
-
-class testAutomataMethods(testAutomata):
-    def runTest(self):
-        self.assertEqual(self.automata.patternSeqPosition("CACAACAA"), [1, 4],"lista de posições errada" )
-        self.assertEqual(self.automata.applyNextState("CACAACAA"), [0, 0, 1, 2, 3, 1, 2, 3, 1],
-                         "lista de próximos estados errada")
-
-unittest.main()
-# States:  4
-# Alphabet:  AC
-# Transition table:
-# 0 , A  ->  1
-# 0 , C  ->  0
-# 1 , A  ->  1
-# 1 , C  ->  2
-# 2 , A  ->  3
-# 2 , C  ->  0
-# 3 , A  ->  1
-# 3 , C  ->  2
-# [0, 0, 1, 2, 3, 1, 2, 3, 1]
-# [1, 4]
-
 
 
