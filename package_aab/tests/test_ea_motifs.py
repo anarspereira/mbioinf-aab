c276772be9b7524377c5e69283fd74a1573ff502
Ana Rafaela Pereira <98289507+anarspereira@users.noreply.github.com>
2022-05-27 22:58:47 +0100

    ea motifs e test: fiz muito por alto, vou continuar popul

--- package_aab/tests/test_ea_motifs.py
+++ package_aab/tests/test_ea_motifs.py
@@ -1,9 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Package dos algoritmos implementados em aula
+Algoritmos Avançados de Bioinformática
+"""
+
+"""
+Unit test: EA Motifs
+"""
+
 import unittest
+from EA_motifs import EAMotifsInt
+from EA_motifs import EAMotifsReal
 
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+class test_EAMotifs(unittest.TestCase):
+    def setUp(self):
+        self.ea_motifs = EA_motifs()
+
+class test_EAMotifsMethods(test_SuffixTree):
+
+    def test_EAMotifsInt(self):
+        x = self.EAMotifsInt(100, 1000, 50, "exemploMotifs.txt") # pedem um ficheiro .txt, está na pasta dos scripts
+        #y =
+        self.assertEqual(x, y)
 
 
 if __name__ == '__main__':
