f34cb544662c94f494ad9f502055cf2c5b7ce17f
Ana Rafaela Pereira <98289507+anarspereira@users.noreply.github.com>
2022-05-27 21:44:46 +0100

    suffix tree: main e testes done

--- package_aab/src/suffix_tree.py
+++ package_aab/src/suffix_tree.py
@@ -1,15 +1,20 @@
 # -*- coding: utf-8 -*-
+"""
+Package dos algoritmos implementados em aula
+Algoritmos Avançados de Bioinformática
+"""
 
 """
 Class: SuffixTree - Árvore de sufixos
-Permite fazer o pré-processamento de uma sequência-alvo, tornando a sua procura mais eficiente.
-É a solução para fazer o pré-processamento de sequências muito grandes, descobrir quais árvores contêm um dado padrão,
-descobrir a substring comum mais longa num conjunto de sequências e calcular o máximo de overlap de um conjunto de sequências.
 """
 
 class SuffixTree:
     """
     Classe que cria árvore de sufixos de um padrão que vai ser procurado numa sequência.
+    Permite fazer o pré-processamento de uma sequência-alvo, tornando a sua procura mais eficiente.
+    É a solução para fazer o pré-processamento de sequências muito grandes, descobrir quais árvores contêm um dado padrão,
+    descobrir a substring comum mais longa num conjunto de sequências e calcular o máximo de overlap de um conjunto de
+    sequências.
     """
 
     def __init__(self): #, seq: str):
@@ -23,7 +28,7 @@ class SuffixTree:
         self.num = 0
         #self.seq = seq
 
-    def print_tree(self) -> None:
+    def printTree(self) -> None:
         """
         Método que imprime a árvore de sufixos.
         """
@@ -33,7 +38,7 @@ class SuffixTree:
             else:
                 print(k, ":", self.nodes[k][0])
 
-    def add_node(self, origin: int, symbol: str, leafnum = -1) -> None:
+    def addNode(self, origin: int, symbol: str, leafnum = -1) -> None:
         """
         Método que adiciona os nós à árvore.
         :param origin: node atual
@@ -46,7 +51,7 @@ class SuffixTree:
         self.nodes[origin][1][symbol] = self.num # cria um novo node e liga-o a um já existente (através do parâmetro origin)
         self.nodes[self.num] = (leafnum, {}) # cria novo node com o número da folha e um dicionário vazio
 
-    def add_suffix(self, p: list, sufnum: int) -> None:
+    def addSuffix(self, p: list, sufnum: int) -> None:
         """
         Método que adiciona sufixo à árvore.
         :param p: padrão
@@ -57,22 +62,23 @@ class SuffixTree:
         while pos < len(p): # enquanto a primeira posição do padrão for menor que o tamanho do padrão,
             if p[pos] not in self.nodes[node][1].keys(): # se a posição inicial do padrão não estiver presente nas keys do dicionário de nodes (se ainda não existir um arco),
                 if pos == len(p) - 1: # se a posição inicial for a última do padrão,
-                    self.add_node(node, p[pos], sufnum) # adiciona um node com o sufixo
+                    self.addNode(node, p[pos], sufnum) # adiciona um node com o sufixo
                 else: # caso contrário,
-                    self.add_node(node, p[pos]) # não adiciona um sufixo
+                    self.addNode(node, p[pos]) # não adiciona um sufixo
             node = self.nodes[node][1][p[pos]] # se existir um arco na posição inicial do padrão, esse passará a ser o current node e a iteração começará deste node
             pos += 1 # incrementar i em 1 para seguir para a próxima iteração, repetindo o processo até chegar ao fim do padrão (len(p)).
 
-    def suffix_tree_from_seq(self, text: str) -> None:
+    def suffixTreeFromSeq(self, text: str) -> None:
         """
-        Método que cria a árvore de sufixos, adicionando um símbolo ("$") no fim da sequência e chama o método anterior para cada sufixo da sequência -> nenhum sufixo será o prefixo de outro sufixo.
+        Método que cria a árvore de sufixos, adicionando um símbolo ("$") no fim da sequência e chama o método anterior
+        para cada sufixo da sequência -> nenhum sufixo será o prefixo de outro sufixo.
         :param text: sequência que será adicionada à árvore
         """
         t = text + "$" # adiciona "$" no fim do texto
         for i in range(len(t)): # para cada índice no range do tamanho do texto (com o $),
-            self.add_suffix(t[i:], i) # adicionar sufixo em cada iteração
+            self.addSuffix(t[i:], i) # adicionar sufixo em cada iteração
 
-    def find_pattern(self, pattern: str):
+    def findPattern(self, pattern: str):
         """
         Método que procura padrões (trie) começando da raiz até chegar ao node final ou falhar a pesquisa.
         :param pattern: padrão a procurar
@@ -85,9 +91,9 @@ class SuffixTree:
                 node = self.nodes[node][1][pattern[pos]] # adicionar o padrão ao node da árvore
             else: # se os arcos não estão presentes no conjunto de símbolos do padrão,
                 return None # retorna none -> o padrão não ocorre
-        return self.get_leaves_below(node) # no fim do ciclo for, retorna o conjunto de folhas abaixo de um dado nó usando o método auxiliar get_leaves_below implementado recursivamente
+        return self.getLeavesBelow(node) # no fim do ciclo for, retorna o conjunto de folhas abaixo de um dado nó usando o método auxiliar get_leaves_below implementado recursivamente
 
-    def get_leaves_below(self, node: int) -> list:
+    def getLeavesBelow(self, node: int) -> list:
         """
         Método auxiliar para colecionar todas as folhas abaixo de um dado nó.
         :param node: node a partir do qual se procuram as informações das folhas abaixo deste
@@ -99,6 +105,6 @@ class SuffixTree:
         else: # caso contrário,
             for k in self.nodes[node][1].keys(): # para cada caracter do node,
                 newnode = self.nodes[node][1][k] # é criado um novo node com esse caracter
-                leaves = self.get_leaves_below(newnode) # é criada uma folha com o novo node
+                leaves = self.getLeavesBelow(newnode) # é criada uma folha com o novo node
                 res.extend(leaves)
         return res
