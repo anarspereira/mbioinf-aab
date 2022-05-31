cf33027af008cd833e026f616a8224f1f9c56f38
camila babo <96503984+camilababo@users.noreply.github.com>
2022-05-31 16:00:24 +0100

    teste overlap #só deu 1

--- package_aab/tests/test_overlapgraph.py
+++ package_aab/tests/test_overlapgraph.py
@@ -4,11 +4,17 @@ from overlap_graph import OverlapGraph
 
 class test_OverlapGraph(unittest.TestCase):
     def setUp(self):
-        seq = "CAATCATGATG"
+        frag = ["ATA", "ACC", "ATG", "ATT", "CAT", "CAT", "CAT", "CCA", "GCA", "GGC", "TAA", "TCA", "TGG", "TTC", "TTT"]
+        self.gr = OverlapGraph(frag, True)
 
-    def composition(self):
-        self.assertEqual(self.composition(3), ['AAT', 'ATC', 'ATG', 'ATG', 'CAA', 'CAT', 'GAT', 'TCA', 'TGA'])
+    def test_validPath(self):
+        path = ["ACC-2", "CCA-8", "CAT-5", "ATG-3", "TGG-13", "GGC-10", "GCA-9", "CAT-6", "ATT-4", "TTT-15", "TTC-14",
+               "TCA-12", "CAT-7", "ATA-1", "TAA-11"]
+        x = self.gr.check_if_valid_path(path)
+        y = True
+        self.assertEqual(x, y)
 
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     unittest.main()
